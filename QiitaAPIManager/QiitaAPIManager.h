f56a162631fa7d0eb0aa7ca680b502aa0af9e7ae
Hiromi Sakurai <sakurai465270@gmail.com>
2018-06-25 21:02:17 +0900

    Initial commit

---
+++ QiitaAPIManager/QiitaAPIManager.h
@@ -0,0 +1,19 @@
+//
+//  QiitaAPIManager.h
+//  QiitaAPIManager
+//
+//  Created by 櫻井寛海 on 2018/06/25.
+//  Copyright © 2018 Hiromi Sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiitaAPIManager.
+FOUNDATION_EXPORT double QiitaAPIManagerVersionNumber;
+
+//! Project version string for QiitaAPIManager.
+FOUNDATION_EXPORT const unsigned char QiitaAPIManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiitaAPIManager/PublicHeader.h>
+
+
