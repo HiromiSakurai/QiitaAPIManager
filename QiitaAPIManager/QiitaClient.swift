5dac25b4f5ddafd367ca05c42ec2fa992acc6f63
Hiromi Sakurai <sakurai465270@gmail.com>
2018-07-07 01:27:50 +0900

    Added QiitaClientProtocol

--- QiitaAPIManager/QiitaClient.swift
+++ QiitaAPIManager/QiitaClient.swift
@@ -9,9 +9,27 @@
 import Foundation
 import RxSwift
 
-public class QiitaClient {
+public protocol QiitaClientProtocol {
+    /// Method which returns single trait of RxSwift.
+    /// To get array of 'Article', subscribe this method.
+    ///
+    /// - Parameters:
+    ///   - searchQuery: Query text for search.
+    ///   - page: For pagination (that return 20 articles per page).
+    /// - Returns: Single<[Article]>
+    func fetchArticles(searchQuery: String, page: Int) -> Single<[Article]>
 
-    let provider: APIRequestProvider = APIRequestProvider.shared
+    /// Method which returns single trait of RxSwift.
+    /// To get item of 'Article', subscribe this method.
+    ///
+    /// - Parameter id: ID of article you want.
+    /// - Returns: Single<Article>
+    func getArticle(with id: String) -> Single<Article>
+}
+
+public class QiitaClient: QiitaClientProtocol {
+
+    private let provider: APIRequestProvider = APIRequestProvider.shared
 
     public func fetchArticles(searchQuery: String, page: Int) -> Single<[Article]> {
         return provider.request(SearchRequest(searchQuery: searchQuery, page: page))
